d275f3ce698f8df665fb86a73124182d54b25a32
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2025-01-02 14:02:58 +0530

    Upload the code of the portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,29 @@
+export const Bio = {
+    name: "Shashin Kalpajith",
+    roles: [
+        // "Software Engineer",
+        "Web Developer",
+        "Full Stack Developer",
+        "MERN Stack Developer",
+        "Programmer",
+        "Undergraduate Student",
+    ],
+    description:
+        "I am a Motivated Software Engineer with six months of experience in MERN stack development, specializing in building high-performance, user-focused web applications. Passionate about problem-solving and continuous learning, with a proactive approach to delivering efficient and impactful software solutions.",
+    github: "https://github.com/Shashin99",
+    resume: " ",
+    linkedin: " ",
+    twitter: " ",
+    insta: " ",
+    facebook: " ",
+};
+
+export const skills = [];
+
+export const experiences = [];
+
+export const education = [];
+
+export const projects = [];
+
+export const TimeLineData = [];
